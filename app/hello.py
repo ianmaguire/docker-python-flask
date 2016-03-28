8dd3e6d8d02eee700e2e7fa05ff9a0be57c512fe
Ian Maguire <imaguire@cisco.com>
2016-03-28 16:54:37 -0700

    Simplified demo app

--- app/hello.py
+++ app/hello.py
@@ -1,12 +1,9 @@
-#!flask/bin/python
-from flask import render_template
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return render_template('hello.html')
+    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run()
